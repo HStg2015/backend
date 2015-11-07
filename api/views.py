09991973e4b854cde69a0565aa362e5beb237c4d
Tim Niederhausen <tim@rnc-ag.de>
2015-11-07 18:02:18 +0100

    Attempt to add help-time filtering

--- api/views.py
+++ api/views.py
@@ -1,5 +1,8 @@
+import datetime
+
 from rest_framework import filters
 from rest_framework import viewsets
+
 from api.models import RefugeeCamp, SimpleOffer, ObjectCategory, HelpTimeSearch
 from api.serializers import RefugeeCampSerializer, SimpleOfferSerializer, ObjectCategorySerializer, HelpTimeSearchSerializer
 
@@ -18,6 +21,21 @@ class SimpleOfferViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.DjangoFilterBackend,)
     filter_fields = ('category',)
 
+class HelpTimeRangeFilter(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        start_time = request.query_params.get('start_time', None)
+        end_time = request.query_params.get('end_time', None)
+
+        if not start_time or not end_time:
+            return queryset
+
+        return queryset.filter(date_time_field__range=(
+            datetime.datetime.combine(start_time, datetime.time.min),
+            datetime.datetime.combine(end_time, datetime.time.max))
+        )
+
 class HelpTimeSearchViewSet(viewsets.ModelViewSet):
     queryset = HelpTimeSearch.objects.all()
     serializer_class = HelpTimeSearchSerializer
+
+    filter_backends = (HelpTimeRangeFilter,)

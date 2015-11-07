39947455b2f89418c14d2266bbe6c20b08cd227b
Tim Niederhausen <tim@rnc-ag.de>
2015-11-07 01:18:26 +0100

    Simple django-rest-framework integration

--- refu/urls.py
+++ refu/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'refu.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )

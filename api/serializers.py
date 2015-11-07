694ec5f13e9b5d367cf8c55e73b3293f0e4f2d7b
Tim Niederhausen <tim@rnc-ag.de>
2015-11-07 01:34:24 +0100

    Fix a typo

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 
 class RefugeeCampSerializer(serializers.Serializer):
-    email = serializers.EmailField()
     city = serializers.CharField()
     postcode = serializers.CharField()
     street = serializers.CharField()

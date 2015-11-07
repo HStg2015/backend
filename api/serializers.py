1a5ab45a7b9ef80094cad89252b38d21d032d51e
Tim Niederhausen <tim@rnc-ag.de>
2015-11-07 02:13:17 +0100

    Implement SimpleOffer saving

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from api.models import SimpleOffer
 
 class RefugeeCampSerializer(serializers.Serializer):
     city = serializers.CharField()
@@ -13,3 +14,22 @@ class SimpleOfferSerializer(serializers.Serializer):
     city = serializers.CharField()
     telephone = serializers.CharField()
     email = serializers.EmailField()
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Snippet` instance, given the validated data.
+        """
+        return SimpleOffer.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Snippet` instance, given the validated data.
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        instance.city = validated_data.get('city', instance.city)
+        instance.telephone = validated_data.get('telephone', instance.telephone)
+        instance.email = validated_data.get('email', instance.email)
+        instance.save()
+        return instance
+    

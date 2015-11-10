e20f914be5f5691ef267b9d8a6b34cab73a13de1
Tim Niederhausen <tim@rnc-ag.de>
2015-11-10 22:16:08 +0100

    Disable debugging in release version

--- refu/settings.py
+++ refu/settings.py
@@ -23,9 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
+DEBUG = False
+TEMPLATE_DEBUG = False
 
 # Application definition
 
